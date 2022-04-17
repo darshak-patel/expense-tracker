021f4693dfcbcae31b0faa78319e49377cdf9987
Darshak <darshak16091997@gmail.com>
2022-04-17 01:14:54 -0400

    added new chart widget class

---
+++ lib/views/chart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Container(),
+    );
+  }
+}
